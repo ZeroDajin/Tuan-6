16f8dd6d3b4526ad4e887c0df4ed8c344d45b8f0
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2024-03-18 14:42:20 +0700

    AAAAAAAAAAAAAA

--- BTNhom/show_tasks.php
+++ BTNhom/show_tasks.php
@@ -5,17 +5,29 @@ $sql = "SELECT * FROM listodo";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "<table class='table'>";
+    echo "<thead class='thead-dark'>";
+    echo "<tr>";
+    echo "<th>Title</th>";
+    echo "<th>Status</th>";
+    echo "<th>Action</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
     while($row = $result->fetch_assoc()) {
-        echo "<li class='list-group-item'>";
-        echo $row['title'];
-        echo "<span class='float-right'>";
-        echo "<a href='update_task_status.php?id=".$row['id']."' class='btn btn-sm btn-info'>Update</a> ";
-        echo "<a href='delete_task.php?id=".$row['id']."' class='btn btn-sm btn-danger' onclick=\"return confirm('Are you sure you want to delete this task?');\">Delete</a>";
-        echo "</span>";
-        echo "</li>";
+        echo "<tr>";
+        echo "<td>".$row['title']."</td>";
+        echo "<td>".($row['completed'] ? 'Completed' : 'Not Completed')."</td>";
+        echo "<td>
+                <a href='update_task_status.php?id=".$row['id']."' class='btn btn-info btn-sm'>Update</a> 
+                <a href='delete_task.php?id=".$row['id']."' class='btn btn-danger btn-sm' onclick=\"return confirm('Are you sure you want to delete this task?');\">Delete</a>
+            </td>";
+        echo "</tr>";
     }
+    echo "</tbody>";
+    echo "</table>";
 } else {
-    echo "<li class='list-group-item'>No tasks found</li>";
+    echo "No tasks found";
 }
 
 include "Dbdisconnection.php";
